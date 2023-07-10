eda3b13ce1783d110094144b0c19cac7a45aa6cd
taranvd <apple.vlad.taran@gmail.com>
2023-07-10 21:37:04 +0300

    replace fetch on axios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import PixabayAPI from './pixabay-api';
 import Notiflix from 'notiflix';
 import throttle from 'lodash.throttle';
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class PixabayAPI {
   #BASE_URL = 'https://pixabay.com/api/?';
   #API_KEY = '32972843-1350190d1db701aee5ffa1a73';
@@ -7,14 +9,19 @@ export default class PixabayAPI {
   perPage = 40;
 
   async fetchPhotos() {
-    const response = await fetch(
-      `${this.#BASE_URL}key=${this.#API_KEY}&q=${
-        this.query
-      }&image_type=photo&orientation=horizontal&safesearch=true&page=${
-        this.page
-      }&per_page=${this.perPage}`
-    );
-    return response.json();
+    try {
+      const response = await axios.get(
+        `${this.#BASE_URL}key=${this.#API_KEY}&q=${
+          this.query
+        }&image_type=photo&orientation=horizontal&safesearch=true&page=${
+          this.page
+        }&per_page=${this.perPage}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching photos:', error);
+      throw error;
+    }
   }
 
   resetPage() {
